9aff75ce7e935ca6613448cf88a08da4d4b08d35
Zachary Whitley <zachary.whitley@tegmentum.com>
2025-09-03 19:40:38 -0400

    chore: remove temporary fix scripts

--- fix_closing_brackets.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Fix incorrectly commented closing brackets
-find wasmtime4j-tests -name "*.java" -exec sed -i '' 's|//     });|          });|g' {} \;
-
-echo "Fixed incorrectly commented closing brackets"
